42b574a228775bb5dae6d9313c293f67bc64f264
Robert Arendac <robertarendac@gmail.com>
2017-03-02 14:36:00 -0800

    Pagination implemented, no navbar yet
    Signed-off-by: Robert Arendac <robertarendac@gmail.com>

--- application/controllers/Mtce.php
+++ application/controllers/Mtce.php
@@ -17,27 +17,56 @@ class Mtce extends Application
 	* map to /welcome/<method_name>
 	* @see https://codeigniter.com/user_guide/general/urls.html
 	*/
+
+	private $items_per_page = 10;
 	
 	public function index()
 	{
+		$this->page(1);	
+	}
+
+	private function show_page($tasks)
+	{
 		$this->data['pagetitle'] = 'TODO List Maintenance';
-		$tasks = $this->tasks->all(); // get all the tasks
 
+	    // build the task presentation output
+	    $result = ''; 	// start with an empty array   
 
-		// substitute the status name
-		foreach ($tasks as $task)
-		if (!empty($task->status))
-		$task->status = $this->statuses->get($task->status)->name;
+	    foreach ($tasks as $task)
+	    {
+	        if (!empty($task->status))
+	            $task->status = $this->statuses->get($task->status)->name;
+	        $result .= $this->parser->parse('oneitem', (array) $task, true);
+	    }
 
-		// build the task presentation output
-		$result = '';   // start with an empty array        
-		foreach ($tasks as $task)
-		$result .= $this->parser->parse('oneitem',(array)$task,true);
+	    $this->data['display_tasks'] = $result;
 
-		// and then pass them on
-		$this->data['display_tasks'] = $result;
-		$this->data['pagebody'] = 'itemlist';
-		$this->render();
-		
+	    // and then pass them on
+	    $this->data['pagebody'] = 'itemlist';
+	    $this->render();
+	}
+
+	// Get a page of items, starting at the beginning
+	function page($pageNum = 1) 
+	{
+		$records = $this->tasks->all();
+		$tasks = array();
+
+		// use a foreach loop, because the record indices may not be sequential
+	    $index = 0; // where are we in the tasks list
+	    $count = 0; // how many items have we added to the extract
+	    $start = ($pageNum - 1) * $this->items_per_page;
+	    foreach($records as $task) 
+	    {
+	        if ($index++ >= $start) 
+	        {
+	            $tasks[] = $task;
+	            $count++;
+	        }
+
+	        if ($count >= $this->items_per_page) 
+	        	break;
+	    }
+	    $this->show_page($tasks);
 	}
 }
